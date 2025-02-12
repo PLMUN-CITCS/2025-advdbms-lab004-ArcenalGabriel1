82527d0ebfc29049d48efd3dde0fefd274c2ee2a
ArcenalGabriel1 <arcenalgabriel_bsit@plmun.edu.ph>
2025-02-12 22:47:58 +0800

    Update create_and_use_db.sql

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,6 +1,7 @@
-CREATE TABLE Students (
-    StudentID INT PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    EnrollmentDate DATE
+USE `UniversityDB`;
+CREATE TABLE IF NOT EXISTS `Students` (
+   `StudentID` INT PRIMARY KEY AUTO_INCREMENT, 
+   `FirstName` VARCHAR(50) NOT NULL,
+   `LastName` VARCHAR(50) NOT NULL,
+   `EnrollmentDate` DATE
 );
